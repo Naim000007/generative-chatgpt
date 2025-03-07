863e94d8365fd2c878a2fbd19a1b7a26f825fd31
Naim000007 <mdnaim01303202218@gmail.com>
2025-03-08 01:26:05 +0600

    loading animation added

--- vite-project/src/components/Main/Main.jsx
+++ vite-project/src/components/Main/Main.jsx
@@ -47,7 +47,13 @@ const Main = () => {
                         </div>
                         <div className="result-data">
                             <img src={assets.gemini_icon} alt="gemini" />
-                            <p dangerouslySetInnerHTML={{ __html: resultData }} />
+                            {loading
+                                ? <div className='loader'>
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                                </div> : <p dangerouslySetInnerHTML={{ __html: resultData }} />}
+
                         </div>
                     </div>
                 )}
